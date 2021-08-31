5067d045ebb4db62d04bdadb40cc7be8d8125adb
Fagner Lima <contato@fagnerlima.pro.br>
2021-08-31 11:57:19 -0300

    Adiciona widget SocialButton

--- split_it/lib/modules/login/login_page.dart
+++ split_it/lib/modules/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:split_it/modules/login/widgets/social_button.dart';
 import 'package:split_it/theme/app_theme.dart';
 
 class LoginPage extends StatefulWidget {
@@ -42,63 +43,25 @@ class _LoginPageState extends State<LoginPage> {
                 ),
               ),
               SizedBox(height: 32,),
-              socialLoginButton(
-                image: Image.asset('assets/images/google.png'),
-                label: 'Entrar com Google'
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32),
+                child: SocialButton(
+                  imagePath: 'assets/images/google.png',
+                  label: 'Entrar com Google'
+                ),
               ),
               SizedBox(height: 12,),
-              socialLoginButton(
-                  image: Image.asset('assets/images/apple.png'),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32),
+                child: SocialButton(
+                  imagePath: 'assets/images/apple.png',
                   label: 'Entrar com Apple'
+                ),
               ),
             ],
           )
         ],
       )
-    );
-  }
-
-  Widget socialLoginButton({
-    required Widget image,
-    required String label
-  }) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 32),
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.fromBorderSide(
-              BorderSide(color: AppTheme.colors.border)
-          ),
-          borderRadius: BorderRadius.circular(5),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Container(
-                width: 56,
-                height: 56,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(width: 15.5,),
-                    image,
-                    SizedBox(width: 15.5,),
-                    Container(
-                      width: 1,
-                      color: AppTheme.colors.border,
-                    )
-                  ],
-                )
-            ),
-            Expanded(child: Container()),
-            Text(
-              label,
-              style: AppTheme.textStyles.button,
-            ),
-            Expanded(child: Container()),
-          ],
-        ),
-      ),
     );
   }
 }
---
+++ split_it/lib/modules/login/widgets/social_button.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:split_it/theme/app_theme.dart';
+
+class SocialButton extends StatelessWidget {
+  final String imagePath;
+  final String label;
+
+  const SocialButton({
+    Key? key,
+    required this.imagePath,
+    required this.label
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.fromBorderSide(
+            BorderSide(color: AppTheme.colors.border)
+        ),
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+              width: 56,
+              height: 56,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(width: 15.5,),
+                  Image.asset(imagePath),
+                  SizedBox(width: 15.5,),
+                  Container(
+                    width: 1,
+                    color: AppTheme.colors.border,
+                  )
+                ],
+              )
+          ),
+          Expanded(child: Container()),
+          Text(
+            label,
+            style: AppTheme.textStyles.button,
+          ),
+          Expanded(child: Container()),
+        ],
+      ),
+    );
+  }
+}
