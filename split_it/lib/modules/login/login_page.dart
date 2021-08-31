69587fab19f1a98875a0f8d097b31a9b49760c03
Fagner Lima <contato@fagnerlima.pro.br>
2021-08-31 11:42:54 -0300

    Adiciona socialLoginButton em LoginPage

--- split_it/lib/modules/login/login_page.dart
+++ split_it/lib/modules/login/login_page.dart
@@ -51,87 +51,67 @@ class _LoginPageState extends State<LoginPage> {
                 ),
               ),
               SizedBox(height: 32,),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 32),
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.fromBorderSide(
-                      BorderSide(color: AppTheme.colors.border)
-                    ),
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Container(
-                        width: 56,
-                        height: 56,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            SizedBox(width: 15.5,),
-                            Image.asset('assets/images/google.png'),
-                            SizedBox(width: 15.5,),
-                            Container(
-                              width: 1,
-                              color: AppTheme.colors.border,
-                            )
-                          ],
-                        )
-                      ),
-                      Expanded(child: Container()),
-                      Text(
-                        'Entrar com Google',
-                        style: GoogleFonts.inter(
-                          color: AppTheme.colors.button,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                      Expanded(child: Container()),
-                    ],
-                  ),
-                ),
-              ),
-              ElevatedButton.icon(
-                onPressed: () {},
-                style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(
-                        AppTheme.colors.backgroundPrimary
-                    )
-                ),
-                icon: Image.asset('assets/images/google.png'),
-                label: Text(
-                  'Entrar com Google',
-                  style: GoogleFonts.inter(
-                    color: AppTheme.colors.button,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
+              socialLoginButton(
+                image: Image.asset('assets/images/google.png'),
+                label: 'Entrar com Google'
               ),
               SizedBox(height: 12,),
-              ElevatedButton.icon(
-                onPressed: () {},
-                style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(
-                        AppTheme.colors.backgroundPrimary
-                    )
-                ),
-                icon: Image.asset('assets/images/apple.png'),
-                label: Text(
-                  'Entrar com Apple',
-                  style: GoogleFonts.inter(
-                    color: AppTheme.colors.button,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
+              socialLoginButton(
+                  image: Image.asset('assets/images/apple.png'),
+                  label: 'Entrar com Apple'
               ),
             ],
           )
         ],
       )
+    );
+  }
+
+  Widget socialLoginButton({
+    required Widget image,
+    required String label
+  }) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 32),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.fromBorderSide(
+              BorderSide(color: AppTheme.colors.border)
+          ),
+          borderRadius: BorderRadius.circular(5),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+                width: 56,
+                height: 56,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(width: 15.5,),
+                    image,
+                    SizedBox(width: 15.5,),
+                    Container(
+                      width: 1,
+                      color: AppTheme.colors.border,
+                    )
+                  ],
+                )
+            ),
+            Expanded(child: Container()),
+            Text(
+              label,
+              style: GoogleFonts.inter(
+                color: AppTheme.colors.button,
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            Expanded(child: Container()),
+          ],
+        ),
+      ),
     );
   }
 }
