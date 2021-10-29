e4ec082ed4af968e5115c9f1f851e0f24a5aee28
Fagner Lima <contato@fagnerlima.pro.br>
2021-10-29 10:56:56 -0300

    Adiciona LoadingWidget em EventTileWidget

--- split_it/lib/modules/home/widgets/event_tile_widget.dart
+++ split_it/lib/modules/home/widgets/event_tile_widget.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:split_it/modules/home/widgets/icon_dollar_widget.dart';
+import 'package:split_it/modules/home/widgets/loading_widget.dart';
 import 'package:split_it/shared/models/event.dart';
 import 'package:split_it/theme/app_theme.dart';
 
 class EventTileWidget extends StatelessWidget {
   final Event data;
+  final bool isLoading;
 
   const EventTileWidget({
     Key? key,
-    required this.data
+    required this.data,
+    this.isLoading = false
   }) : super(key: key);
 
   IconDollarType get iconDollarType => data.value >= 0
@@ -17,6 +20,40 @@ class EventTileWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    if (isLoading) {
+      return Row(
+        children: [
+          LoadingWidget(size: Size(48, 48)),
+          Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 16),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    ListTile(
+                      contentPadding: EdgeInsets.zero,
+                      title: LoadingWidget(size: Size(81, 19)),
+                      subtitle: LoadingWidget(size: Size(52, 18)),
+                      trailing: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          LoadingWidget(size: Size(61, 17)),
+                          SizedBox(height: 5,),
+                          LoadingWidget(size: Size(44, 18)),
+                        ],
+                      ),
+                    ),
+                    Divider(
+                      color: AppTheme.colors.divider,
+                    ),
+                  ],
+                ),
+              )
+          )
+        ],
+      );
+    }
+
     return Row(
       children: [
         IconDollarWidget(type: iconDollarType),
