d3f06c8ca2f8feef44089ee1c6dc022ed7c766b2
Fagner Lima <fagnerlima91@gmail.com>
2022-03-11 11:34:43 -0300

    Adiciona mobx no CreateSplitController

---
+++ split_it/lib/modules/create_split/create_split_controller.g.dart
@@ -0,0 +1,58 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'create_split_controller.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
+
+mixin _$CreateSplitController on _CreateSplitController, Store {
+  final _$currentPageAtom = Atom(name: '_CreateSplitController.currentPage');
+
+  @override
+  int get currentPage {
+    _$currentPageAtom.reportRead();
+    return super.currentPage;
+  }
+
+  @override
+  set currentPage(int value) {
+    _$currentPageAtom.reportWrite(value, super.currentPage, () {
+      super.currentPage = value;
+    });
+  }
+
+  final _$_CreateSplitControllerActionController =
+      ActionController(name: '_CreateSplitController');
+
+  @override
+  dynamic backPage() {
+    final _$actionInfo = _$_CreateSplitControllerActionController.startAction(
+        name: '_CreateSplitController.backPage');
+    try {
+      return super.backPage();
+    } finally {
+      _$_CreateSplitControllerActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  dynamic nextPage() {
+    final _$actionInfo = _$_CreateSplitControllerActionController.startAction(
+        name: '_CreateSplitController.nextPage');
+    try {
+      return super.nextPage();
+    } finally {
+      _$_CreateSplitControllerActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  String toString() {
+    return '''
+currentPage: ${currentPage}
+    ''';
+  }
+}
