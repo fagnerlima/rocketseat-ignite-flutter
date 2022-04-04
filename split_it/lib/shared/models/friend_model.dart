a05686f74355678731bf02d4e5c8cd2abe361158
Fagner Lima <fagnerlima91@gmail.com>
2022-04-04 20:31:36 -0300

    Adiciona FriendModel::photoURL

--- split_it/lib/shared/models/friend_model.dart
+++ split_it/lib/shared/models/friend_model.dart
@@ -2,30 +2,50 @@ import 'package:split_it/shared/models/base_model.dart';
 
 class FriendModel extends BaseModel {
   String name;
+  String photoURL;
 
   FriendModel({
     required this.name,
+    required this.photoURL,
   });
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      (other is FriendModel &&
+          runtimeType == other.runtimeType &&
+          name == other.name &&
+          photoURL == other.photoURL);
+
+  @override
+  int get hashCode => name.hashCode ^ photoURL.hashCode;
+
+  @override
+  String toString() {
+    return 'FriendModel{' + ' name: $name,' + ' photoURL: $photoURL,' + '}';
+  }
+
+  FriendModel copyWith({
+    String? name,
+    String? photoURL,
+  }) {
+    return FriendModel(
+      name: name ?? this.name,
+      photoURL: photoURL ?? this.photoURL,
+    );
+  }
 
   Map<String, dynamic> toMap() {
     return {
       'name': this.name,
+      'photoURL': this.photoURL,
     };
   }
 
   factory FriendModel.fromMap(Map<String, dynamic> map) {
     return FriendModel(
       name: map['name'] as String,
+      photoURL: (map['photoURL'] ?? '') as String,
     );
   }
-
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is FriendModel &&
-          runtimeType == other.runtimeType &&
-          name == other.name;
-
-  @override
-  int get hashCode => name.hashCode;
 }
